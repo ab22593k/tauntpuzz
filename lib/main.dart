8e248e64f471f414d314d2e267363d625df98862
Roaa94 <roaa94khaddam@gmail.com>
2025-03-25 20:07:11 +0100

    chore: update project

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,10 @@ import 'package:dashtronaut/app.dart';
 import 'package:dashtronaut/services/service_locator.dart';
 import 'package:dashtronaut/services/storage/storage_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() {
+  usePathUrlStrategy();
   setupServiceLocator();
   runZonedGuarded<Future<void>>(() async {
     final StorageService storageService = getIt<StorageService>();
